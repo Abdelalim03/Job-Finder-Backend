1831830e89e018ada5e63e3704099da60465ecee
SifEddine05 <sifousamir72@gmail.com>
2024-05-03 23:23:53 +0100

    create ,update,delete task

--- src/routes/tasks.route.js
+++ src/routes/tasks.route.js
@@ -10,7 +10,6 @@ const { verifyTasker } = require("../middlewares/roles.middleware.js");
 
 // categoriesRouter.route("/create").post(authenticateToken, verifyAdmin,createCategory)
 tasksRouter.route("/create").post(authenticateToken,upload.array('taskimages'), verifyTasker,createTaskRules,tasksValidator,createTask)
-// i need to add verification that who update or delete is the owner 
 tasksRouter.route("/update/:id").put(authenticateToken, verifyTasker,updateTaskRules,tasksValidator,updateTask) //i need to add to the upload the images and deleted imgaes send in a list 
 tasksRouter.route("/delete/:id").delete(authenticateToken, verifyTasker,deleteTask)
 
